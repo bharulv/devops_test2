dba7065e5f6286540dbc1c5e5c329b660ad1f11f
Bharul Varma <bharulv.nethority@gmail.com>
2025-07-08 12:19:17 +0530

    testing_first_commit

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@ if ( !function_exists( 'child_theme_configurator_css' ) ):
         wp_enqueue_script( 'custom-js', trailingslashit( get_stylesheet_directory_uri()) . 'custom.js', array( 'jquery' ), null, true);
     }
 endif;
-add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 )
+add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
 // END ENQUEUE PARENT ACTION
 
@@ -108,4 +108,4 @@ function category_tabs_shortcode($atts) {
     <?php
     return ob_get_clean(); // Return the buffered content
 }
-add_shortcode('category_tabs', 'category_tabs_shortcode');
+add_shortcode('category_tabs', 'category_tabs_shortcode')
